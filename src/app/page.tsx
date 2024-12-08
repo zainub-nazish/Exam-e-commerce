80878b43a14c55df183bee7be82f7a3980891903
zainub-nazish <160176214+zainub-nazish@users.noreply.github.com>
2024-12-08 22:40:10 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Card from "@/components/Card";
 import Paraimage from "@/components/Paraimage";
 import Trending from "@/components/Trending";
 
+
 function Home() {
   return (
     <div>
---
+++ src/app/pages.tsx/index.js
@@ -0,0 +1,5 @@
+
+
+export default function Home() {
+    return <h1>Welcome to Chair E-Commerce!</h1>;
+}
